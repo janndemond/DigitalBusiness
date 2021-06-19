390474556bafc853485b0bb70d462f3a3e76e7f6
Jann Demond <janndemond@gmail.com>
2021-06-19 15:15:27 +0200

    Init final Download from Marzipano

---
+++ data.js
@@ -0,0 +1,573 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-0_welcome",
+      "name": "0_Welcome",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.39789043630882226,
+        "pitch": 0.013891795673314888,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.663738525401449,
+          "pitch": -0.06340843067462032,
+          "rotation": 0,
+          "target": "1-1_technopark"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2590035583247463,
+          "pitch": 0.13462481453125008,
+          "title": "Willkommen",
+          "text": "Hoi Freunde!&nbsp;<div>Vielen Dank, dass Ihr gekommen seid, um uns zu helfen. Wir haben ein riesen Problem! In wenigen Tagen findet das Fürstenfest statt, eines der größten Feste in Liechtenstein. Und dieses Jahr haben wir sogar die großartige Möglichkeit bekommen, unseren Gin dort exklusiv anzubieten. Die Vorbereitungen laufen schon seit Wochen auf Hochtouren. Und jetzt das! Unser neuer Trainee, der sich hauptsächlich um die Organisation für das Fest gekümmert hat, ist verschwunden. Wie vom Erdboden verschluckt! Er reagiert nicht auf Anrufe, und wir machen uns langsam große Sorgen. Das letzte mal haben wir ihn gestern gesehen, als wir noch in unserer Lieblingsbar auf das kommende Fest angestoßen haben. Aber jetzt ist alles in Gefahr. Wir müssen sie unbedingt finden, hoffentlich ist ihr nichts zugestoßen. Könnt ihr uns helfen?</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-1_technopark",
+      "name": "1_TechnoPark",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3607972399495516,
+        "pitch": 0.04050045858059015,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2869804435436496,
+          "pitch": 0.08423357660425523,
+          "rotation": 0,
+          "target": "2-2_bar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.01889285633430937,
+          "pitch": -0.0031486119751988895,
+          "title": "To-Do List",
+          "text": "Bild"
+        },
+        {
+          "yaw": 0.18978832867153983,
+          "pitch": 0.2985218736834092,
+          "title": "Start",
+          "text": "Link"
+        },
+        {
+          "yaw": -0.2644852542788687,
+          "pitch": 0.3267736355491131,
+          "title": "Kalender",
+          "text": "Bild"
+        },
+        {
+          "yaw": 1.2356663112103057,
+          "pitch": -0.16037961303617898,
+          "title": "Technopark",
+          "text": "Link"
+        },
+        {
+          "yaw": 2.580603638276358,
+          "pitch": 0.3154586124823098,
+          "title": "Staatsfeiertag&nbsp;",
+          "text": "Link"
+        }
+      ]
+    },
+    {
+      "id": "2-2_bar",
+      "name": "2_Bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.686833842570014,
+        "pitch": 0.13664173890441234,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8582616467756843,
+          "pitch": 0.38569157928499465,
+          "rotation": 0,
+          "target": "3-3_empfang"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1039706669128222,
+          "pitch": 0.08511261683053384,
+          "title": "Franco",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.18415190458643949,
+          "pitch": 0.15136275006445743,
+          "title": "Bredi",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.18256522452617396,
+          "pitch": 0.0896249543679204,
+          "title": "Daniel",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.28371297827221476,
+          "pitch": 0.1577632480028086,
+          "title": "Trainee",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.655919939364308,
+          "pitch": 0.1684465529114636,
+          "title": "Alwalser",
+          "text": "Song"
+        },
+        {
+          "yaw": -2.6798710941977575,
+          "pitch": 0.4244032358950349,
+          "title": "Gin Preparation",
+          "text": "Video"
+        },
+        {
+          "yaw": 2.7777952912129127,
+          "pitch": -0.022748173125471283,
+          "title": "Leider",
+          "text": "Das ist leider nicht der richtige Gin. Finde die&nbsp; 32Peaks Flasche, um einen Hinweis zu erhalten."
+        },
+        {
+          "yaw": 2.9952626078212594,
+          "pitch": 0.29654201279860004,
+          "title": "Barkeeper&nbsp;",
+          "text": "Audio"
+        },
+        {
+          "yaw": -2.8466466396787524,
+          "pitch": -0.024763954489261053,
+          "title": "Super",
+          "text": "Sehr gut, du hast den 32Peaks Gin gefunden. Hinweis: wir befinden uns in hier: Link zur Bar"
+        },
+        {
+          "yaw": -2.5771915614857797,
+          "pitch": -0.03083415930365163,
+          "title": "<span style=\"font-family: Calibri, sans-serif; font-size: 14.6667px; background-color: rgba(58, 68, 84, 0.8);\">Leider&nbsp;</span>",
+          "text": "<span style=\"font-size:11.0pt;line-height:107%;\nfont-family:&quot;Calibri&quot;,sans-serif;mso-ascii-theme-font:minor-latin;mso-fareast-font-family:\nCalibri;mso-fareast-theme-font:minor-latin;mso-hansi-theme-font:minor-latin;\nmso-bidi-font-family:&quot;Times New Roman&quot;;mso-bidi-theme-font:minor-bidi;\nmso-ansi-language:DE;mso-fareast-language:EN-US;mso-bidi-language:AR-SA\">Das\nist leider nicht der richtige Gin. Finde die&nbsp;\n32Peaks Flasche, um einen Hinweis zu erhalten.</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-3_empfang",
+      "name": "3_Empfang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4577390940087067,
+        "pitch": 0.0023143283261752856,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46399332284544137,
+          "pitch": 0.10428788172190195,
+          "rotation": 0,
+          "target": "4-4_destillerie"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11248885890118743,
+          "pitch": -0.018041266782374166,
+          "title": "Corona Zahlen",
+          "text": "Link"
+        },
+        {
+          "yaw": 0.15970471709390743,
+          "pitch": 0.26294752202400673,
+          "title": "Tonic",
+          "text": "Link"
+        },
+        {
+          "yaw": 1.0309298301490735,
+          "pitch": -0.20675490088442317,
+          "title": "32Peaks",
+          "text": "TikTok"
+        }
+      ]
+    },
+    {
+      "id": "4-4_destillerie",
+      "name": "4_Destillerie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5471268927543953,
+        "pitch": 0.4625290735844363,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6572057505611006,
+          "pitch": 0.03203315956985975,
+          "rotation": 0,
+          "target": "5-5_steinraum"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2624008759186651,
+          "pitch": -0.15474014096907673,
+          "title": "Gin",
+          "text": "Link"
+        },
+        {
+          "yaw": -2.156249840863614,
+          "pitch": 0.3686509475444204,
+          "title": "Radio",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.4155677168752803,
+          "pitch": 0.3126013877889413,
+          "title": "Mate",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-5_steinraum",
+      "name": "5_Steinraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.03540531610454423,
+        "pitch": 0.32863462231768636,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.757164369142603,
+          "pitch": 0.049097057935203026,
+          "rotation": 0,
+          "target": "6-6_panorama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.11027622002190718,
+          "pitch": 0.7742016550309572,
+          "title": "LieMobiel",
+          "text": "Bild"
+        }
+      ]
+    },
+    {
+      "id": "6-6_panorama",
+      "name": "6_Panorama",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2638539033505527,
+        "pitch": 0.10993026984077936,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43996007274554927,
+          "pitch": 1.3722135929806054,
+          "rotation": 0,
+          "target": "2-2_bar"
+        },
+        {
+          "yaw": 0.8997122222876524,
+          "pitch": -0.1989241659073251,
+          "rotation": 0,
+          "target": "7-7_fuerstensteig"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6625805215547764,
+          "pitch": -0.06499558151755025,
+          "title": "Gamprin",
+          "text": "Link"
+        },
+        {
+          "yaw": -0.34751600814345096,
+          "pitch": 0.08545254546010739,
+          "title": "Schaan",
+          "text": "Link"
+        },
+        {
+          "yaw": -0.00918477098680981,
+          "pitch": -0.36321445396964336,
+          "title": "3 Schwestern",
+          "text": "Link"
+        },
+        {
+          "yaw": 0.531771785695188,
+          "pitch": -0.34726227150779465,
+          "title": "Fürstensteig&nbsp;",
+          "text": "Link"
+        },
+        {
+          "yaw": 1.1140273464094328,
+          "pitch": -0.04243193570859205,
+          "title": "Uni",
+          "text": "Link"
+        },
+        {
+          "yaw": -1.672158146697031,
+          "pitch": -0.0790873619934267,
+          "title": "Hoher Kasten",
+          "text": "Link"
+        },
+        {
+          "yaw": 1.823983500354168,
+          "pitch": -0.060442956124965974,
+          "title": "Pizol",
+          "text": "Link"
+        }
+      ]
+    },
+    {
+      "id": "7-7_fuerstensteig",
+      "name": "7_Fuerstensteig",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5356471134968421,
+        "pitch": 0.8961600090394146,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6594364262852039,
+          "pitch": 0.9493790442877756,
+          "rotation": 0,
+          "target": "7-7_fuerstensteig"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-8_fuerstensteig",
+      "name": "8_Fuerstensteig",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6271982411693102,
+        "pitch": 0.05695394378480856,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.023614855060834827,
+          "pitch": 0.3035737951697861,
+          "rotation": 0,
+          "target": "9-9_ende"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-9_ende",
+      "name": "9_Ende",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 2.0712129587699266,
+        "pitch": 0.15737162655371328,
+        "fov": 1.4041062726189781
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Exit Game - 32 Peaks",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
